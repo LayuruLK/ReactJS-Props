ee53707d4b262dbdad783bc19dd0e872eb1c7592
Layuru Karunathilaka <139636618+LayuruLK@users.noreply.github.com>
2024-11-22 13:00:38 +0000

    Import Child to App.js

--- Day07/myapp/src/App.js
+++ Day07/myapp/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { useState} from 'react';
+import Childcom from './Childcom';
 
 function App() {
   const [childname, setChildname] = useState("Default");
 
   return (
     <div className="App">
-      
+      <h2>Hello React I'm Parent</h2>
+      <Childcom/>
     </div>
   );
 }
